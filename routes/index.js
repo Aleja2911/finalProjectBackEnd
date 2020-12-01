891e48481fb518b972cfba96ab4092b9c533c851
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-12-01 14:52:01 +0100

    first draft

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello this is my project')
+  res.send('find-your-science')
   // res.render('index', { title: 'Express' });
 });
 
